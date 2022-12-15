72d6bc68caa49c225269aefcead60b68437ee3b4
Hussein Beygi <hussein1997.hb@hotmail.com>
2022-12-15 19:50:12 +0330

    Edit : Add Jwt Profiler

--- IdentityManagers/ProfileManager.cs
+++ IdentityManagers/ProfileManager.cs
@@ -1,4 +1,6 @@
-﻿using CRM.Services.Identity.Models;
+﻿using CRM.Services.Identity.Infrastructure.Middlewares;
+using CRM.Services.Identity.Infrastructure.Settings;
+using CRM.Services.Identity.Models;
 using IdentityServer4.Models;
 using IdentityServer4.Services;
 using Microsoft.AspNetCore.Identity;
@@ -9,27 +11,41 @@ namespace CRM.Services.Identity.IdentityManagers
 	public class ProfileManager : IProfileService
 	{
 		private readonly UserManager<User> _userManager;
+		private readonly ApplicationSettings _applicationSettings;
+		private readonly IHttpContextAccessor _httpContextAccessor;
 
-		public ProfileManager(UserManager<User> userManager)
+		public ProfileManager(UserManager<User> userManager, ApplicationSettings applicationSettings, IHttpContextAccessor httpContextAccessor)
 		{
 			_userManager = userManager;
+			_applicationSettings = applicationSettings;
+			_httpContextAccessor = httpContextAccessor;
 		}
 
 		public async Task GetProfileDataAsync(ProfileDataRequestContext context)
 		{
 			var user = await _userManager.GetUserAsync(context.Subject);
 
+
+			var supportedCultureNames =
+				_applicationSettings.CultureSettings.SupportedCultureNames;
+
+			var defaultCultureName =
+					_applicationSettings.CultureSettings.DefaultCultureName;
+
+			var currentCookieCulture = CultureCookieHandlerMiddleware
+				.GetCultureNameByCookie(_httpContextAccessor.HttpContext, supportedCultureNames);
+
 			var claims = new List<Claim>
 			{
-				new Claim(ClaimTypes.NameIdentifier, user?.UserName ?? "NoNameIdentifier"),
+				new Claim(ClaimTypes.NameIdentifier, user?.Id.ToString() ?? "NoNameIdentifier"),
+				new Claim(ClaimTypes.Actor, user?.UserName ?? "NoActor"),
 				new Claim(ClaimTypes.GivenName, user?.FirstName ?? "NoGivenName"),
 				new Claim(ClaimTypes.Surname, user?.LastName ?? "NoSurname"),
 				new Claim(ClaimTypes.Name, string.Concat(user?.FirstName," ",user?.LastName)),
-				new Claim("OrganizationId", user.OrganizationId.ToString() ?? default),
-				new Claim("BusinessUnitId", user.BusinessUnitId.ToString() ?? default),
-				new Claim(ClaimTypes.Locality, Thread.CurrentThread.CurrentCulture.Name),
-				new Claim("CurrentUICulture", Thread.CurrentThread.CurrentUICulture.Name),
-
+				new Claim("oid", user.OrganizationId.ToString() ?? default),
+				new Claim("buid", user.BusinessUnitId.ToString() ?? default),
+				new Claim(ClaimTypes.Locality,currentCookieCulture ?? defaultCultureName),
+				new Claim("CurrentUICulture",currentCookieCulture ?? defaultCultureName)
 			};
 
 			context.IssuedClaims.AddRange(claims);
